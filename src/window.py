991f6d25582486a173d7f3b391c980375dba7cff
Martin Pobaschnig <mpobaschnig@protonmail.com>
2023-01-12 11:16:58 +0100

    Implement model download
    Closes: https://github.com/mpobaschnig/imagery/issues/8

--- src/window.py
+++ src/window.py
@@ -17,20 +17,32 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from gi.repository import Adw
-from gi.repository import Gtk
-from gi.repository import GLib
+from gi.repository import Gtk, GLib, Adw, Gio
+from gettext import gettext as i18n
+
+import os
+import logging
+from enum import Enum
 
 
 @Gtk.Template(resource_path='/io/github/mpobaschnig/Imagery/window.ui')
 class ImageryWindow(Adw.ApplicationWindow):
+    class DownloadState(Enum):
+        Download = 0,
+        Cancel = 1,
+
     __gtype_name__ = 'ImageryWindow'
 
-    textview = Gtk.Template.Child()
-    scrolled_window = Gtk.Template.Child()
+    stack: Gtk.Stack = Gtk.Template.Child()
+    progress_bar: Gtk.ProgressBar = Gtk.Template.Child()
+    download_model_button: Gtk.Button = Gtk.Template.Child()
+    model_license_hint_label: Gtk.Label = Gtk.Template.Child()
+
+    download_state: DownloadState = DownloadState.Download
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self._init_start()
 
     @Gtk.Template.Callback()
     def _on_run_button_clicked(self, _button):
@@ -42,4 +54,74 @@ class ImageryWindow(Adw.ApplicationWindow):
 
     @Gtk.Template.Callback()
     def _on_download_model_button_clicked(self, _button):
-        pass
+        if self.download_state == self.DownloadState.Cancel:
+            self.download_cancellable.cancel()
+            self._toggle_download_state()
+            return
+
+        download_path = os.path.join(GLib.get_user_data_dir(),
+                                     "v2-1_768-ema-pruned.ckpt")
+        download_uri = "https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt"
+
+        model_uri_file: Gio.File = Gio.File.new_for_uri(download_uri)
+        model_file = Gio.File.new_for_path(download_path)
+        self.download_cancellable = Gio.Cancellable.new()
+        model_uri_file.copy_async(model_file,
+                                  Gio.FileCopyFlags.OVERWRITE,
+                                  GLib.PRIORITY_DEFAULT, self.download_cancellable,
+                                  self._on_download_progress_cb, (),
+                                  self._on_download_finished_cb, None)
+
+        self._toggle_download_state()
+
+    def _on_download_progress_cb(self, current_num_bytes, total_num_bytes):
+        fraction = current_num_bytes / total_num_bytes
+        self.progress_bar.set_fraction(fraction)
+
+        curr_gib = current_num_bytes / 1024 / 1024 / 1024
+        total_gib = total_num_bytes / 1024 / 1024 / 1024
+        self.progress_bar.set_text(
+            f"{round(curr_gib,1)} GiB / {round(total_gib, 1)} GiB"
+        )
+
+    def _toggle_download_state(self):
+        if self.download_state == self.DownloadState.Download:
+            self.download_model_button.remove_css_class("suggested-action")
+            self.download_model_button.add_css_class("destructive-action")
+            self.download_model_button.set_label(i18n("Cancel"))
+
+            self.download_state = self.DownloadState.Cancel
+
+            self.progress_bar.set_text(i18n("Initializing..."))
+            self.progress_bar.set_show_text(True)
+            self.progress_bar.set_fraction(0.0)
+            self.progress_bar.set_visible(True)
+
+            self.model_license_hint_label.set_visible(False)
+        else:
+            self.download_model_button.remove_css_class("destructive-action")
+            self.download_model_button.add_css_class("suggested-action")
+            self.download_model_button.set_label(i18n("Download Model"))
+
+            self.download_state = self.DownloadState.Download
+
+            self.progress_bar.set_visible(False)
+
+            self.model_license_hint_label.set_visible(True)
+
+    def _on_download_finished_cb(self, file, result, user_data):
+        try:
+            file.copy_finish(result)
+        except GLib.Error as e:
+            logging.error(e)
+
+    def _init_start(self) -> None:
+        model_path = os.path.join(GLib.get_user_data_dir(),
+                                  "v2-1_768-ema-pruned.ckpt")
+
+        if os.path.exists(model_path) and os.path.isfile(model_path):
+            logging.debug(f"Model exists at path: {model_path}")
+            self.stack.set_visible_child_name("text_to_image")
+        else:
+            logging.debug(f"Model does not exist at path: {model_path}")
+            self.stack.set_visible_child_name("start")
