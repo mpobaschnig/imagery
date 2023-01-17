ed51db69245499e9bec92ae0dbb26622f5b13dda
Martin Pobaschnig <mpobaschnig@protonmail.com>
2023-01-17 13:32:07 +0100

    main: Increase logging level
    We need some more info.

--- src/main.py
+++ src/main.py
@@ -19,6 +19,7 @@
 
 import sys
 import gi
+import logging
 
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
@@ -36,6 +37,8 @@ class ImageryApplication(Adw.Application):
         self.create_action('quit', self.quit, ['<primary>q'])
         self.create_action('about', self.on_about_action)
         self.create_action('preferences', self.on_preferences_action)
+
+        logging.getLogger().setLevel(logging.INFO)
 
     def do_activate(self):
         """Called when the application is activated.
