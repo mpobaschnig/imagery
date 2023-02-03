6053a9cfbedd13ea04c315f155fc5c7f41d8cf88
Martin Pobaschnig <mpobaschnig@protonmail.com>
2023-02-03 16:32:13 +0100

    start_page: Implement DownloadManager
    StartPage will be the entry of the application, so this is the only
    location where things get downloaded.

--- src/start_page.py
+++ src/start_page.py
@@ -23,84 +23,79 @@ from enum import Enum
 from gettext import gettext as i18n
 from typing import Optional
 
-from gi.repository import Gio, GLib, Gtk
+from gi.repository import GObject, Gio, GLib, Gtk
+
+from .download_manager import DownloadManager
+from .model_files import sd15_files, sd15_folder
 
 
 @Gtk.Template(resource_path='/io/github/mpobaschnig/Imagery/start_page.ui')
 class StartPage(Gtk.Box):
-    class DownloadState(Enum):
-        DOWNLOAD = 0
-        CANCEL = 1
-
     __gtype_name__ = "StartPage"
+
+    __gsignals__ = {
+        "finished": (GObject.SignalFlags.RUN_FIRST, None, ())
+    }
 
     _progress_bar: Gtk.ProgressBar = Gtk.Template.Child()
     _download_model_button: Gtk.Button = Gtk.Template.Child()
+    _cancel_download_button: Gtk.Button = Gtk.Template.Child()
+    _continue_button: Gtk.Button = Gtk.Template.Child()
     _model_license_hint_label: Gtk.Label = Gtk.Template.Child()
 
-    _download_state: DownloadState = DownloadState.DOWNLOAD
-    _download_cancellable: Optional[Gio.Cancellable] = None
+    def __init__(self):
+        super().__init__()
+
+        self._download_manager: DownloadManager = DownloadManager(sd15_files)
+
+        self._download_manager.connect("reset", self._reset)
+        self._download_manager.connect("update", self._update)
+        self._download_manager.connect("cancelled", self._cancelled)
+        self._download_manager.connect("finished", self._finished)
+
+    def _reset(self, download_manager: DownloadManager):
+        self._progress_bar.set_show_text(i18n("Initializing..."))
+        self._progress_bar.set_fraction(0.0)
+        self._progress_bar.set_visible(True)
+
+    def _update(self,
+                _download_manager: DownloadManager,
+                fraction: float,
+                current_downloaded: int,
+                total_size: int,
+                unit: str,
+                current_index: int,
+                total_files: int):
+        self._progress_bar.set_text(i18n(
+            f"{current_downloaded} {unit} / {total_size} {unit} - ({current_index} of {total_files})"
+        ))
+        self._progress_bar.set_fraction(fraction)
+
+    def _cancelled(self, download_manager: DownloadManager):
+        self._progress_bar.set_visible(False)
+        self._download_model_button.set_visible(True)
+        self._cancel_download_button.set_visible(False)
+        self._model_license_hint_label.set_visible(True)
+
+    def _finished(self, download_manager: DownloadManager):
+        self._progress_bar.set_fraction(100)
+        self._progress_bar.set_text(i18n("Download finished."))
+        self._continue_button.set_visible(True)
+        self._cancel_download_button.set_visible(False)
 
     @Gtk.Template.Callback()
     def _on_download_model_button_clicked(self, _button):
-        if self._download_state == self.DownloadState.CANCEL:
-            self._download_cancellable.cancel()
-            self._toggle_download_state()
-            return
+        self._cancel_download_button.set_visible(True)
+        self._download_model_button.set_visible(False)
 
-        download_path = os.path.join(GLib.get_user_data_dir(),
-                                     "v2-1_768-ema-pruned.ckpt")
-        # pylint: disable-next=line-too-long
-        download_uri = "https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt"  # noqa: E501
+        self._model_license_hint_label.set_visible(False)
 
-        model_uri_file: Gio.File = Gio.File.new_for_uri(download_uri)
-        model_file = Gio.File.new_for_path(download_path)
-        self._download_cancellable = Gio.Cancellable.new()
-        model_uri_file.copy_async(model_file,
-                                  Gio.FileCopyFlags.OVERWRITE,
-                                  GLib.PRIORITY_DEFAULT, self._download_cancellable,
-                                  self._on_download_progress_cb, (),
-                                  self._on_download_finished_cb, None)
+        self._download_manager.start()
 
-        self._toggle_download_state()
+    @Gtk.Template.Callback()
+    def _on_cancel_download_button_clicked(self, _button):
+        self._download_manager.cancel()
 
-    def _on_download_progress_cb(self, current_num_bytes, total_num_bytes):
-        fraction = current_num_bytes / total_num_bytes
-        self._progress_bar.set_fraction(fraction)
-
-        curr_gib = current_num_bytes / 1024 / 1024 / 1024
-        total_gib = total_num_bytes / 1024 / 1024 / 1024
-        self._progress_bar.set_text(
-            f"{round(curr_gib,1)} GiB / {round(total_gib, 1)} GiB"
-        )
-
-    def _toggle_download_state(self):
-        if self._download_state == self.DownloadState.DOWNLOAD:
-            self._download_model_button.remove_css_class("suggested-action")
-            self._download_model_button.add_css_class("destructive-action")
-            self._download_model_button.set_label(i18n("Cancel"))
-
-            self._download_state = self.DownloadState.CANCEL
-
-            self._progress_bar.set_text(i18n("Initializing..."))
-            self._progress_bar.set_show_text(True)
-            self._progress_bar.set_fraction(0.0)
-            self._progress_bar.set_visible(True)
-
-            self._model_license_hint_label.set_visible(False)
-        else:
-            self._download_model_button.remove_css_class("destructive-action")
-            self._download_model_button.add_css_class("suggested-action")
-            self._download_model_button.set_label(i18n("Download Model"))
-
-            self._download_state = self.DownloadState.DOWNLOAD
-
-            self._progress_bar.set_visible(False)
-
-            self._model_license_hint_label.set_visible(True)
-
-    def _on_download_finished_cb(self, file, result, user_data):
-        try:
-            file.copy_finish(result)
-        except GLib.Error as error:
-            logging.error(error)
+    @Gtk.Template.Callback()
+    def _on_continue_button_clicked(self, _button):
+        self.emit("finished")
