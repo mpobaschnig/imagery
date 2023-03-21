83f2a66cc4c381f50c43890acbc50240674e5e31
Martin Pobaschnig <mpobaschnig@protonmail.com>
2023-03-21 18:46:21 +0100

    t2i: Don't set maximum width
    It's hard to decide what's better, but remove it for now.

--- src/text_to_image_page.py
+++ src/text_to_image_page.py
@@ -301,10 +301,6 @@ class TextToImagePage(Gtk.Box):
 
         img_width = img.get_paintable().get_intrinsic_width()
         img_height = img.get_paintable().get_intrinsic_height()
-        maximum_width = 300
-        if img_width > maximum_width:
-            img_height = img_height / (img_width / maximum_width)
-            img_width = maximum_width
 
         overlay.set_size_request(img_width, img_height)
         overlay.set_child(img)
